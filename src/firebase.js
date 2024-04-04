f57078ae08c31c246b898d3ba389c8bd2d72c3ba
pleaseworking <firebaseforeshop@gmail.com>
2024-04-04 08:08:36 +0700

    real firebase have usage limit need to develop on firebase emulator instead

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAEE5mekvCS4K3u6eeDLQPuDWG212xEr6A',
+    authDomain: 'mvc-eshop-test.firebaseapp.com',
+    databaseURL: 'https://mvc-eshop-test-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'mvc-eshop-test',
+    storageBucket: 'mvc-eshop-test.appspot.com',
+    messagingSenderId: '711455833282',
+    appId: '1:711455833282:web:8feae4ee892b16e6b10f5b'
+  }
+
+  const app = initializeApp(firebaseConfig)
+
+  const db = getFirestore(app)
+  connectFirestoreEmulator(db, '127.0.0.1', 8081)
+
+  export {
+    db
+  }
--- src/stores/user/product.js
+++ src/stores/user/product.js
@@ -1,14 +1,18 @@
 import { defineStore } from 'pinia'
 
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/firebase'
+
 export const useProductStore = defineStore('product', {
   state: () => ({
     list: []
   }),
   actions: {
-    loadProducts () {
-      const products = localStorage.getItem('admin-products')
-      if (products) {
-        this.list = JSON.parse(products)
+    async loadProducts () {
+      const productSnapshot = await getDocs(collection(db, 'products'))
+      const products = productSnapshot.docs.map(doc => doc.data())
+      if (products.length > 0) {
+        this.list = products
         this.loaded = true
       }
     },    
